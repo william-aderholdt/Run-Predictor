6243481f880347fc53eec066636102982f09d3bf
william-aderholdt <40444366+william-aderholdt@users.noreply.github.com>
2018-12-18 16:00:45 -0600

    began c# programming

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace runPredictor
+{
+    class Program
+    {
+        static string[] userSettings;
+
+        static void Main(string[] args)
+        {
+            ReadFromFile();
+            WebScrapeWeather(userSettings);
+        }
+
+        static string[] ReadFromFile()
+        {
+            // string[] userSettings = System.IO.File.ReadAllLines(Environment.CurrentDirectory + "\\settings.txt");
+
+            string[] userSettings = System.IO.File.ReadAllLines(@"C:\Users\David\Documents\projects\runPredictor\settings.txt");
+            
+            foreach(var item in userSettings) //remove later
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            return userSettings;
+        }
+
+        static void WebScrapeWeather(string[] userSettings)
+        {
+            Console.WriteLine(userSettings[0].Substring(9));
+            Console.ReadLine(); //remove later
+            // 'https://weather.com/weather/hourbyhour/l/ + userSettings[0] +:4:US'
+        }
+
+    }
+
+
+}
